a8557fc6feefb80f5281566a430340577d4e8b6c
meir meir <maornet@gmail.com>
2023-05-11 14:06:32 +0000

    Finish celery introduction

--- course4_proj/settings.py
+++ course4_proj/settings.py
@@ -48,6 +48,7 @@ class Dev(Configuration):
         'django.contrib.sessions',
         'django.contrib.messages',
         'django.contrib.staticfiles',
+        "django_celery_results",
         "movies",
         "gh",
     ]
@@ -159,4 +160,7 @@ class Dev(Configuration):
         },
     }
 
-    OMDB_KEY = "1234" # values.SecretValue()
+    OMDB_KEY = "" # values.SecretValue()
+
+    CELERY_RESULT_BACKEND = "django-db"
+    CELERY_BROKER_URL = "redis://localhost:6379/0"
